499e4e716f2b1492052cf62350c6fdeb04fce909
dibkb <dibas9110@gmail.com>
2025-04-27 21:25:59 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { createEffect, createSignal, type Component } from "solid-js";
+
+const App: Component = () => {
+  const [count, setCount] = createSignal<number>(0);
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
+  const [isEven, setIsEven] = createSignal<boolean>(false);
+  createEffect(() => {
+    if (count() % 2 == 0) {
+      setIsEven(true);
+    } else {
+      setIsEven(false);
+    }
+  });
+  return (
+    <div class={""}>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <p>{count()}</p>
+      <p>{isEven() ? "Even" : "Odd"}</p>
+    </div>
+  );
+};
+
+export default App;
